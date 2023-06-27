067fecd326d405cc6879a303d89b0ecea6f7d08d
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-06-27 21:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-window.promises =[];
-// Do not change the code above this
-// add your promises to the array `promises`
+window.promises=[];
 let a = new Promise(resolve =>{
 	setTimeout(() =>{
 		resolve("a")
@@ -17,7 +15,7 @@ let c = new Promise(resolve =>{
 	},3000)
 })
 let d = new Promise(resolve =>{
-	setTimeout(() => {
+	setTimeout(() =>{
 		resolve("d")
 	},4000)
 })
@@ -26,9 +24,44 @@ let e = new Promise(resolve =>{
 		resolve("e")
 	},5000)
 })
-let x= Promise.any([a, b, c, d, e]);
-window.promises = [a, b, c, d, e];
-x.then((data) => {
+let x = Promise.any([a, b ,c, d, e]);
+window.promises=[a, b ,c ,d , e];
+x.then((data) =>{
 	const element = document.getElementById("output");
-	element.innnerText= data;
-})
+	element.innerText= data;
+});
+
+// window.promises =[];
+// // Do not change the code above this
+// // add your promises to the array `promises`
+// let a = new Promise(resolve =>{
+// 	setTimeout(() =>{
+// 		resolve("a")
+// 	},1000)
+// })
+// let b = new Promise(resolve =>{
+// 	setTimeout(() =>{
+// 		resolve("b")
+// 	},2000)
+// })
+// let c = new Promise(resolve =>{
+// 	setTimeout(() =>{
+// 		resolve("c")
+// 	},3000)
+// })
+// let d = new Promise(resolve =>{
+// 	setTimeout(() => {
+// 		resolve("d")
+// 	},4000)
+// })
+// let e = new Promise(resolve =>{
+// 	setTimeout(() =>{
+// 		resolve("e")
+// 	},5000)
+// })
+// let x= Promise.any([a, b, c, d, e]);
+// window.promises = [a, b, c, d, e];
+// x.then((data) => {
+// 	const element = document.getElementById("output");
+// 	element.innnerText= data;
+// })
